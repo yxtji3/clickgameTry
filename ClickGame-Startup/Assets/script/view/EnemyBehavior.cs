55402a55d9d7ed668ddab6f709d19e199f1e2c35
4419-4-8\user <yxtji3@gmail.com>
2017-12-14 21:52:50 +0800

    1214_version

---
+++ ClickGame-Startup/Assets/script/view/EnemyBehavior.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Animator))]//animator綁定
+[RequireComponent(typeof(MeshFader))]
+public class EnemyBehavior : MonoBehaviour {
+
+    private Animator animator;
+    private MeshFader meshFader;
+    private AudioSource audiosource;
+	
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        meshFader = GetComponent<MeshFader>();
+        audiosource = GetComponent<AudioSource>();
+    }
+
+    private void OnEnable() {
+        StartCoroutine(meshFader.FadeIn());
+    }
+
+    [ContextMenu("DeDemage")]
+    public void Update()
+    {
+        if (Input.GetButtonDown("Fire1")) { 
+        DoDemage(10);
+    }
+    }
+
+    public void DoDemage(int attack)
+    {
+        animator.SetTrigger("hurt");
+        audiosource.Play();
+    }
+
+    public IEnumerator Execute()
+    {
+        yield return null;
+        
+        }
+
+}
